aef21a6eca96e93ffaaf089d25f3ac4f56a2b016
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-06-04 03:56:55 +0900

    cmd: simplify `run` command

--- cmd/run.go
+++ cmd/run.go
@@ -1,25 +1,10 @@
 package cmd
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"strings"
-	"sync"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/athena"
-	"github.com/aws/aws-sdk-go/service/athena/athenaiface"
-	"github.com/skatsuta/athenai/exec"
-	"github.com/skatsuta/athenai/print"
+	"github.com/skatsuta/athenai/athenai"
 	"github.com/spf13/cobra"
-)
-
-const (
-	tickInterval = 1000 * time.Millisecond
 )
 
 // runCmd represents the run command
@@ -53,80 +38,5 @@ func runRun(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	// Create a service configuration
-	cfg := aws.NewConfig().WithRegion(config.Region)
-
-	// Set log level
-	if config.Debug {
-		cfg = cfg.WithLogLevel(aws.LogDebug | aws.LogDebugWithHTTPBody | aws.LogDebugWithRequestErrors)
-	} else {
-		// Surpress log outputs
-		log.SetOutput(ioutil.Discard)
-	}
-
-	// Create an Athena client
-	client := athena.New(session.Must(session.NewSession(cfg)))
-
-	// Split SQL statements by semicolons
-	stmts := strings.Split(args[0], ";")
-
-	// Create channels
-	resultCh := make(chan *exec.Result)
-	errCh := make(chan error)
-	doneCh := make(chan struct{})
-	var wg sync.WaitGroup
-
-	// Print running messages
-	if !config.Silent {
-		go func() {
-			tick := time.Tick(tickInterval)
-			fmt.Print("Running query")
-			for {
-				select {
-				case <-tick:
-					fmt.Print(".")
-				}
-			}
-		}()
-	}
-
-	// Run each statement concurrently using goroutine
-	for _, stmt := range stmts {
-		query := stmt // capture locally
-		if strings.TrimSpace(query) == "" {
-			continue // Skip empty statements
-		}
-		wg.Add(1)
-		go runQuery(client, query, resultCh, errCh, &wg)
-	}
-
-	// Monitoring goroutine to notify that all the query executions have finished
-	go func() {
-		wg.Wait()
-		doneCh <- struct{}{}
-	}()
-
-	for {
-		select {
-		case r := <-resultCh:
-			fmt.Print("\n")
-			print.NewTable(os.Stdout).Print(r)
-		case e := <-errCh:
-			fmt.Print("\n")
-			fmt.Fprintln(os.Stderr, e)
-		case <-doneCh:
-			return
-		}
-	}
-}
-
-func runQuery(client athenaiface.AthenaAPI, query string, resultCh chan *exec.Result, errCh chan error, wg *sync.WaitGroup) {
-	// Run a query, and send results or an error
-	r, err := exec.NewQuery(client, query, &config.QueryConfig).Run()
-	if err != nil {
-		errCh <- err
-	} else {
-		resultCh <- r
-	}
-	wg.Done()
+	athenai.New(os.Stdout, config).RunQuery(args[0])
 }
