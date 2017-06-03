addd173b81e0d5bf842c7b98cca7ab7ef0ebe6c1
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-06-04 03:52:30 +0900

    athenai: add Athenai#RunQuery method

--- athenai/athenai.go
+++ athenai/athenai.go
@@ -1,76 +1,27 @@
 package athenai
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/athena"
+	"github.com/aws/aws-sdk-go/service/athena/athenaiface"
 	"github.com/skatsuta/athenai/exec"
+	"github.com/skatsuta/athenai/print"
 )
 
 const (
 	tickInterval = 1000 * time.Millisecond
 )
-
-// Config is a configuration information for Athenai.
-type Config struct {
-	exec.QueryConfig
-	Debug  bool
-	Region string
-	Silent bool
-}
-
-// Athenai is a main struct to run this app.
-type Athenai struct {
-	out      io.Writer
-	cfg      *Config
-	client   *athena.Athena
-	interval time.Duration
-}
-
-// New creates a new Athena.
-func New(out io.Writer, cfg *Config) *Athenai {
-	if cfg == nil {
-		cfg = &Config{}
-	}
-
-	a := &Athenai{
-		out:      out,
-		cfg:      cfg,
-		client:   newClient(cfg),
-		interval: tickInterval,
-	}
-	return a
-}
-
-func (a *Athenai) print(x ...interface{}) {
-	fmt.Fprint(a.out, x...)
-}
-
-// ShowProgressMsg shows progress messages while queries are being executed.
-func (a *Athenai) ShowProgressMsg() {
-	// Do nothing if it's run in silent mode
-	if a.cfg.Silent {
-		return
-	}
-
-	// Start a new goroutine to show progress messages regularly
-	go func() {
-		a.print("Running query")
-		tick := time.Tick(a.interval)
-		for {
-			select {
-			case <-tick:
-				a.print(".")
-			}
-		}
-	}()
-}
 
 // newClient creates a new Athena client.
 func newClient(cfg *Config) *athena.Athena {
@@ -86,4 +37,145 @@ func newClient(cfg *Config) *athena.Athena {
 	}
 
 	return athena.New(session.Must(session.NewSession(c)))
+}
+
+// splitStmts splits SQL statements in the query by semicolons.
+// It drops empty statements.
+func splitStmts(query string) []string {
+	splitted := strings.Split(query, ";")
+
+	// Filtering without allocating: https://github.com/golang/go/wiki/SliceTricks#filtering-without-allocating
+	stmts := splitted[:0]
+	for _, s := range splitted {
+		// Select non-empty statements
+		if strings.TrimSpace(s) != "" {
+			stmts = append(stmts, s)
+		}
+	}
+
+	return stmts
+}
+
+// Config is a configuration information for Athenai.
+type Config struct {
+	exec.QueryConfig
+	Debug  bool
+	Region string
+	Silent bool
+}
+
+// Athenai is a main struct to run this app.
+type Athenai struct {
+	out io.Writer
+
+	cfg    *Config
+	client athenaiface.AthenaAPI
+	// tick interval
+	interval time.Duration
+
+	resultCh chan *exec.Result
+	errCh    chan error
+	doneCh   chan struct{}
+	wg       sync.WaitGroup
+	mu       sync.Mutex
+}
+
+// New creates a new Athena.
+func New(out io.Writer, cfg *Config) *Athenai {
+	a := &Athenai{
+		out:      out,
+		cfg:      cfg,
+		client:   newClient(cfg),
+		interval: tickInterval,
+		resultCh: make(chan *exec.Result, 1),
+		errCh:    make(chan error, 1),
+		doneCh:   make(chan struct{}),
+	}
+	return a
+}
+
+func (a *Athenai) print(x ...interface{}) {
+	fmt.Fprint(a.out, x...)
+}
+
+// showProgressMsg shows progress messages while queries are being executed.
+func (a *Athenai) showProgressMsg(ctx context.Context) {
+	a.print("Running query")
+	tick := time.Tick(a.interval)
+	for {
+		select {
+		case <-tick:
+			a.print(".")
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// runSingleQuery runs a single query. `query` must be a single SQL statement.
+func (a *Athenai) runSingleQuery(query string) {
+	// Run a query, and send results or an error
+	r, err := exec.NewQuery(a.client, query, &a.cfg.QueryConfig).Run()
+	if err != nil {
+		a.errCh <- err
+	} else {
+		a.resultCh <- r
+	}
+}
+
+// monitorComplete waits for all query executions and notifies `doneCh` when they are all complete.
+func (a *Athenai) monitorComplete() {
+	a.wg.Wait()
+	a.doneCh <- struct{}{}
+}
+
+// RunQuery runs the given query.
+// It splits the query by semicolons and run each statement concurrently.
+// It skips empty statements.
+func (a *Athenai) RunQuery(query string) {
+	// Split statements
+	stmts := splitStmts(query)
+	if len(stmts) == 0 {
+		a.print("Nothing executed")
+		return
+	}
+
+	// Print progress messages
+	if !a.cfg.Silent {
+		ctx, cancel := context.WithCancel(context.Background())
+		// Stop printing when this method finishes
+		defer cancel()
+		go a.showProgressMsg(ctx)
+	}
+
+	// Run each statement concurrently
+	var wg sync.WaitGroup
+	wg.Add(len(stmts))
+	for _, stmt := range stmts {
+		go func(query string) {
+			a.runSingleQuery(query)
+			wg.Done()
+		}(stmt) // Copy stmt to use in goroutines
+	}
+
+	doneCh := make(chan struct{})
+
+	// Monitoring goroutine to wait for all query executions and notifies `doneCh` when they are all complete.
+	go func() {
+		wg.Wait()
+		doneCh <- struct{}{}
+	}()
+
+	for {
+		select {
+		case r := <-a.resultCh:
+			a.print("\n")
+			print.NewTable(a.out).Print(r)
+		case e := <-a.errCh:
+			a.print("\n")
+			fmt.Fprintln(os.Stderr, e)
+		case <-doneCh:
+			return
+		}
+	}
 }
--- athenai/athenai_test.go
+++ athenai/athenai_test.go
@@ -2,23 +2,119 @@ package athenai
 
 import (
 	"bytes"
+	"context"
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go/service/athena"
+	"github.com/skatsuta/athenai/internal/stub"
+	"github.com/skatsuta/athenai/internal/testhelper"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSplitStmts(t *testing.T) {
+	tests := []struct {
+		query   string
+		wantLen int
+	}{
+		{"", 0},
+		{";", 0},
+		{"; ; \n \t \r   ;", 0},
+		{"   ; SELECT;   ; ", 1},
+		{`	;
+			SELECT *
+			FROM test
+			WHERE id = 1;
+			SHOW
+			TABLES;
+			   ;
+			`, 2},
+	}
+
+	for _, tt := range tests {
+		got := splitStmts(tt.query)
+
+		assert.Len(t, got, tt.wantLen, "Query: %q")
+	}
+}
+
 func TestShowProgressMsg(t *testing.T) {
-	want := "Running query.." // Message shown during 2 ticks
+	want := "Running query."
 
 	var out bytes.Buffer
 	a := &Athenai{
 		out:      &out,
 		cfg:      &Config{},
-		interval: 10 * time.Millisecond,
+		interval: 1 * time.Millisecond,
 	}
-	a.ShowProgressMsg()
-	<-time.After(29 * time.Millisecond) // Wait for less than 30 ms (3 ticks)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	defer cancel()
+	a.showProgressMsg(ctx)
 
-	assert.Equal(t, want, out.String())
+	assert.Contains(t, out.String(), want)
+}
+
+const showDatabasesOutput = `
+SHOW DATABASES;
++-----------------+
+| cloudfront_logs |
+| elb_logs        |
+| sampledb        |
++-----------------+
+Run time: 1.00 seconds | Data scanned: 1.00 KB
+`
+
+func TestRunQuery(t *testing.T) {
+	tests := []struct {
+		query    string
+		id       string
+		rs       athena.ResultSet
+		execTime int64
+		scanned  int64
+		want     string
+	}{
+		{
+			query: "",
+			id:    "TestRunQuery_EmptyStmt1",
+			want:  "Nothing executed",
+		},
+		{
+			query: "  ; ;  ",
+			id:    "TestRunQuery_EmptyStmt2",
+			want:  "Nothing executed",
+		},
+		{
+			query: "SHOW DATABASES",
+			id:    "TestRunQuery_ShowDBs",
+			rs: athena.ResultSet{
+				Rows: testhelper.CreateRows(
+					[][]string{
+						{"cloudfront_logs"},
+						{"elb_logs"},
+						{"sampledb"},
+					},
+				),
+			},
+			execTime: 1000,
+			scanned:  1000,
+			want:     showDatabasesOutput,
+		},
+	}
+
+	var out bytes.Buffer
+	for _, tt := range tests {
+		a := New(&out, &Config{Silent: true})
+		client := stub.NewClient(tt.id)
+		client.ResultSet = tt.rs
+		stats := new(athena.QueryExecutionStatistics).
+			SetEngineExecutionTimeInMillis(tt.execTime).
+			SetDataScannedInBytes(tt.scanned)
+		client.QueryExecution.SetStatistics(stats).SetQuery(tt.query)
+		a.client = client
+		a.RunQuery(tt.query)
+
+		assert.Equal(t, tt.want, out.String(), "Query: %q, Id: %s", tt.query, tt.id)
+
+		out.Reset()
+	}
 }
