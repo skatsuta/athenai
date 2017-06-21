236a63e2711d986b9393fcb96aa795ef226bf06a
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-06-22 00:40:29 +0900

    cmd: show millisecond level timestamps and line numbers of source files

--- cmd/root.go
+++ cmd/root.go
@@ -31,6 +31,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 		if !config.Debug {
 			// Disable debug log messages
 			log.SetOutput(ioutil.Discard)
