b9cf0c12c8a7dda2a3ceb9391169ccf503ff1b10
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-07-03 22:40:16 +0900

    internal/bytes: add Buffer struct that is goroutine-safe

---
+++ internal/bytes/buffer.go
@@ -0,0 +1,27 @@
+// Package bytes provides goroutine-safe Buffer for testing-only purpose.
+package bytes
+
+import (
+	"bytes"
+	"sync"
+)
+
+// A Buffer is a variable-sized buffer of bytes with Read and Write methods.
+// The zero value for Buffer is an empty buffer ready to use.
+// This is goroutine-safe for Write and String method.
+type Buffer struct {
+	mu sync.Mutex
+	bytes.Buffer
+}
+
+func (b *Buffer) Write(p []byte) (int, error) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	return b.Buffer.Write(p)
+}
+
+func (b *Buffer) String() string {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	return b.Buffer.String()
+}
